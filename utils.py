a1a148267acd1c16f07e3f9f3087e5993d163028
SrVladyslav <vlama@inf.upv.es>
2021-01-30 10:47:08 +0100

    Hello world

---
+++ utils.py
@@ -0,0 +1,15 @@
+########################################################################################
+# 
+#   Code created for the University Hack Datathon 2021
+#   Team HakunaDatata 
+#
+########################################################################################
+
+def scrap_site(link):
+    """
+    Given a link to a website, returns its full content
+    :param link: Link to a website
+    :return content: Dataframe with the page content
+    """
+    pass # Scrapy or BeautifulSoup
+
